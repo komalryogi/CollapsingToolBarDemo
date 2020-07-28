dfd2ef2f7e7de04c606e93a7a23a402d4feaa88e
komalryogi <komalblyogi@gmail.com>
2020-07-28 11:09:48 +0400

    second

--- app/src/main/java/com/komal/collapsetoolbardemo/MainActivity.kt
+++ app/src/main/java/com/komal/collapsetoolbardemo/MainActivity.kt
@@ -2,10 +2,40 @@ package com.komal.collapsetoolbardemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.AppBarLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun initCollapsingToolbar() {
+        ctl_slider.title = ""
+        app_bar_layout.setExpanded(true)
+
+        // hiding & showing the title when toolbar expanded & collapsed
+        app_bar_layout.addOnOffsetChangedListener(object : AppBarLayout.OnOffsetChangedListener {
+            var isShow = false
+            var scrollRange = -1
+            override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.totalScrollRange
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    try {
+                        ctl_slider.title = ""
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                        ctl_slider.title = getString(R.string.app_name)
+                    }
+                    isShow = true
+                } else if (isShow) {
+                    ctl_slider.title = " "
+                    isShow = false
+                }
+            }
+        })
     }
 }
